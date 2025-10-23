df147b6765f6b9e7969e87d55eed96991eb31e71
Amogh <princeamogh7.gmail.com>
2025-10-23 20:05:13 +0530

    Fix translation in TabChange component by passing language parameter

--- src/component/TabChange/TabChange.jsx
+++ src/component/TabChange/TabChange.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import "./TabChange.scss"
 import { t } from "../../translations"
+import { useLanguage } from "../../context/LanguageContext"
 
 const TabChange = ({
     tabList, setStep, step
 }) => {
+    const { language } = useLanguage();
     return (
         <div className='tablist'>
             {tabList && tabList.map((data, key) => (
@@ -12,7 +14,7 @@ const TabChange = ({
                     <button
                     className={step == data.step ? "active" : "inactive"}
                       onClick={() => setStep(data.step)}
-                      >{t(data.name)}</button>
+                      >{t(data.name, language)}</button>
                 </div>
             ))
             }
